fca08fca8deaaaee0d5f5e35f1e14be7eb9d8a6e
María José Cubero Hidalgo <majo.qbro@gmail.com>
2020-05-10 19:32:18 -0600

    Lab 2 last changes

--- Laboratorio2/lab2.py
+++ Laboratorio2/lab2.py
@@ -8,15 +8,18 @@ from math import e
 
 #Laboratorio 1 PRUEBA
 
-input_size = 1
-hidden_sizes = [1]
-output_size = 1
-
-model = torch.nn.Sequential(
-# Hidden Layer 
-torch.nn.Linear(input_size, hidden_sizes[0]),
-torch.nn.Sigmoid(),
-torch.nn.Linear(hidden_sizes[0], output_size))
+def model_fn(input_size, hidden_sizes, output_size):
+    print('input_size %.2f, hidden_sizes %.2f, output_size %.2f'%(input_size, hidden_sizes[0], output_size))
+    return nn.Sequential(
+    # Hidden Layer 
+    nn.Linear(input_size, hidden_sizes[0]),
+    
+    #nn.ReLU(),
+    #nn.Sigmoid(),
+    #nn.Tanh(),
+    #nn.Softplus(),
+    nn.Hardtanh(),
+    nn.Linear(hidden_sizes[0], output_size))
 
 class Network(nn.Module):
     def __init__(self):
@@ -47,6 +50,15 @@ def read_csv(fname, colx, coly):
     APMTensor = torch.tensor(data[coly])
     return actionLatencyTensor, APMTensor
 
+# Obtain data
+def read_csv_2(fname, APM, action_latency, total_map_exp, workers_made, uniq_units_made, compl_units_made, compl_abil_used):
+    data = pd.read_csv(fname)
+    columns_tensor = torch.tensor([[data[action_latency]], [data[total_map_exp]], 
+                                   [data[workers_made]], [data[uniq_units_made]], 
+                                   [data[compl_units_made]],[data[compl_abil_used]]])
+    APM_tensor = torch.tensor(data[APM])
+    return columns_tensor, APM_tensor
+
 def create_tensors(tensor, indexes_perm):
 	#create tensor 1
 	tensor1 = tensor[:30]
@@ -64,60 +76,18 @@ def create_tensors(tensor, indexes_perm):
 
 ## Third part: Fitting a Linear Model with Gradient Descent
 
-def model_nonlin(a, c, x, b):
-    return (a * x**b + c)
-
-def model_nonlin_e(a, c, x, b):
-    return (a * e**(b*x) + c)
-
 def loss_fn(y, y_calculated):
     return ((y - y_calculated)**2).mean()
 
-def dmodel_w(x,y,w,b):
-    return ((-2 * x) * (y - (w * x + b))).mean()
-
-def dmodel_b(x,y,w,b):
-    return (-2 * (y - (w * x + b))).mean()
-
-'''def training_auto(n, w, b, alpha, x, y):
-    w.requires_grad=True
-    b.requires_grad=True
-    
-    w_vector= ([0]*n)
-    b_vector= ([0]*n)
+def training_opt_SGD(n, alpha, x, y, input_size, hidden_sizes, output_size):
     loss= ([0]*n)
-    
-    for i in range(n):
-        y_calculated= model2(x.view(len(x), 1))
-        print('AAAA ', y_calculated)
-        loss[i]= loss_fn(y,y_calculated)
-        loss[i].backward()
-        w= w - (w.grad * alpha)
-        b= b - (b.grad * alpha)
-        
-        # Save values to print them
-        w_vector[i]= w.item()
-        b_vector[i]= b.item()
-        
-        w.detach_()
-        w.requires_grad_()
-        b.detach_()
-        b.requires_grad_()
-        
-    return w_vector, b_vector, loss'''
-
-def training_opt_SGD(n, w, b, alpha, x, y):
-    #w.requires_grad=True
-    #b.requires_grad=True
-    
-    loss= ([0]*n)
+    model = model_fn(input_size, hidden_sizes, output_size)
     optimizer = torch.optim.SGD(model.parameters(), lr= alpha)
+    matrix = x.view(-1, input_size)
     
     for i in range(n):
        
-        
-        y_calculated= model(x.view(len(x), 1))
-        #print('y_calculated', y_calculated)
+        y_calculated= model(matrix)
         
         loss[i]= loss_fn(y, y_calculated)
         
@@ -125,36 +95,18 @@ def training_opt_SGD(n, w, b, alpha, x, y):
         loss[i].backward()
         optimizer.step()
         
-    return w, b, loss
-
-def show_results_table(w, b, loss):
-    data= pd.concat([pd.DataFrame(loss), pd.DataFrame(w), pd.DataFrame(b)], 1)
-    data.columns= ['loss', 'w','b']
-    print(data)
+    print('loss -> ', loss[-1].item())
+    if input_size <= 1:   
+        show_plot(x, model, hidden_sizes[0], loss[-1])
+    return loss[-1]
     
-def show_results_table_opt(w, b, loss):
-    print("w --> ", w.item())
-    print("b --> ", b.item())
-    data= pd.DataFrame(loss)
-    data.columns= ['loss']
-    print(data)
-    
-def show_training_plot(w, b, x, y, name, last_loss):
-    line = w[-1] * x + b[-1]
-    plt.scatter(x, y, color= 'g')
-    plt.plot(x, line)
-    plt.title("Training results: " + name + ". Last loss: " + last_loss)
+def show_plot(x, model, hidden_sizes, loss):
+    n =  5
+    new_x = torch.linspace(0, 4, n)
+    plt.title("Predicted function. Hidden size: %.2f. Loss: %.2f"%(int(hidden_sizes), loss))
+    y = model(new_x.view(n, 1))
+    plt.plot(new_x.detach(), y.detach().numpy(), c = 'g')
     plt.show()
-    
-def show_training_plot_opt(w, b, x, y, name, last_loss):
-    line = w.item() * x + b.item()
-    plt.scatter(x, y, color= 'g')
-    plt.plot(x, line)
-    plt.title("Training results: " + name + ". Last loss: " + last_loss)
-    plt.show()
-
-def get_xn(x):
-    return (x - x.mean())/((x.max() - x.min())/2)
 
 def main(datafile):
     
@@ -165,29 +117,57 @@ def main(datafile):
     tensor1_AL, tensor2_AL, tensor3_AL = create_tensors(AL_tensor, indexes_perm)
     tensor1_APM, tensor2_APM, tensor3_APM = create_tensors(APM_tensor, indexes_perm)
     
-    it = 1000
-    w_tensor = torch.tensor(float(-1))
-    b_tensor = torch.tensor(float(225))
+    it = 50
     alpha_value = float(0.000001)
     
-    xn = get_xn(tensor3_AL)
+    x = tensor3_AL
     
-    '''
-    print("\n---Traninig auto (Xn)---\n")
-    w, b, loss= training_auto(it, w_tensor, b_tensor, alpha_value, xn, tensor3_APM)
-    show_training_plot(w, b, tensor3_AL, tensor3_APM, " training set (Xn)- auto: 80%", str(loss[-1].item()))
-    show_training_plot(w, b, tensor1_AL, tensor1_APM, " test set (Xn)- auto: 30 vals", str(loss[-1].item()))
-    show_training_plot(w, b, tensor2_AL, tensor2_APM, " test set (Xn)- auto: 20%", str(loss[-1].item()))
-    show_results_table(w, b, loss)
-    '''
+    print("\n---Traninig opt SGD ---\n")
+    input_size = 1
+    hidden_sizes = [1]
+    output_size = 1
     
-    print('model.parameter(): ', model.parameters())
-    print("\n---Traninig opt SGD (Xn)---\n")
-    w, b, loss= training_opt_SGD(it, w_tensor, b_tensor, alpha_value, xn, tensor3_APM)
-    show_training_plot_opt(w, b, tensor3_AL, tensor3_APM, " training set opt->SGD (Xn): 80%", str(loss[-1].item()))
-    show_training_plot_opt(w, b, tensor1_AL, tensor1_APM, " test set opt->SGD (Xn): 30 vals", str(loss[-1].item()))
-    show_training_plot_opt(w, b, tensor2_AL, tensor2_APM, " test set opt->SGD (Xn): 20%", str(loss[-1].item()))
-    show_results_table_opt(w, b, loss)
+    training_opt_SGD(it, alpha_value, x, tensor3_APM, input_size, hidden_sizes, output_size)
+    
+    print("\n---Traninig opt SGD ---\n")
+    input_size = 1
+    hidden_sizes = [10]
+    output_size = 1
+    training_opt_SGD(it, alpha_value, x, tensor3_APM, input_size, hidden_sizes, output_size)
+    
+    print("\n---Traninig opt SGD ---\n")
+    input_size = 1
+    hidden_sizes = [20]
+    output_size = 1
+    training_opt_SGD(it, alpha_value, x, tensor3_APM, input_size, hidden_sizes, output_size)
+    
+    print("\n---Traninig opt SGD ---\n")
+    input_size = 1
+    hidden_sizes = [2000]
+    output_size = 1
+    training_opt_SGD(it, alpha_value, x, tensor3_APM, input_size, hidden_sizes, output_size)
+    
+    #Second part
+    x_tensor, y_tensor = read_csv_2(datafile, "APM", "ActionLatency", "TotalMapExplored", 
+                                            "WorkersMade", "UniqueUnitsMade", "ComplexUnitsMade", "ComplexAbilitiesUsed")
+    
+    print("\n---Traninig opt SGD (6 inputs)---\n")
+    input_size = 6
+    hidden_sizes = [1]
+    output_size = 1
+    training_opt_SGD(it, alpha_value, x_tensor, y_tensor, input_size, hidden_sizes, output_size)
+    
+    print("\n---Traninig opt SGD (6 inputs)---\n")
+    input_size = 6
+    hidden_sizes = [10]
+    output_size = 1
+    training_opt_SGD(it, alpha_value, x_tensor, y_tensor, input_size, hidden_sizes, output_size)
+    
+    print("\n---Traninig opt SGD (6 inputs)---\n")
+    input_size = 6
+    hidden_sizes = [20]
+    output_size = 1
+    training_opt_SGD(it, alpha_value, x_tensor, y_tensor, input_size, hidden_sizes, output_size)
 
     
 if __name__ == "__main__":
